ddd437b0b20d0107e79685a3660c9d72f4675442
bruceyboy86 <bruced247@gmail.com>
2021-03-10 21:46:11 +0000

    added deleteTodo, finished todos/index.ts, code formatter

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,22 +1,22 @@
 import { ITodo } from "./../types/todo";
 import { model, Schema } from "mongoose";
 
-const todoSchema: Schema = new Schema (
-    {
-        name:{
-            type: String,
-            required: true
-        },
-        description:{
-            type: String,
-            required: true
-        },
-        status:{
-            type: Boolean,
-            required: true
-        }
+const todoSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
-)
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export default model<ITodo>("Todo", todoSchema)
+export default model<ITodo>("Todo", todoSchema);
