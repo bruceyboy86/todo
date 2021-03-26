2b4916edf644f62d06ba9f645de522717d657db4
bruceyboy86 <bruced247@gmail.com>
2021-03-26 21:18:24 +0000

    wrote handleSaveTodo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,6 +16,17 @@ const App: React.FC = () => {
     .catch((err: Error) => console.log(err))
   }
 
+  const handleSaveTodo = (e: React.FormEvent, formData: ITodo): void => {
+    e.preventDefault()
+    addTodo(formData)
+      .then(({status,data}) =>{
+        if (status !== 201) {
+          throw new Error("Error! Todo not saved");
+        }
+        setTodos(data.todos)
+      })
+      .catch(err => console.log(err))
+  }
   return null
 }
 
